e51668d97f38f16cc7d59b9dd32893f0094b830a
Benedikt Groß <bg@benedikt-gross.de>
2021-06-28 10:04:35 +0200

    Flash RGB LED if a Filler was detected

--- um-uh-ah-detector-arduino.ino
+++ um-uh-ah-detector-arduino.ino
@@ -1,24 +1,10 @@
-/* Edge Impulse Arduino examples
- * Copyright (c) 2021 EdgeImpulse Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+// LEDs
+#define PIN_LED     (13u)
+#define LED_BUILTIN PIN_LED
+#define LEDR        (22u)
+#define LEDG        (23u)
+#define LEDB        (24u)
+#define LED_PWR     (25u)
 
 // If your target is limited in memory remove this macro to save 10K RAM
 #define EIDSP_QUANTIZE_FILTERBANK   0
@@ -28,7 +14,7 @@
  * with slices per model window set to 4. Results in a slice size of 250 ms.
  * For more info: https://docs.edgeimpulse.com/docs/continuous-audio-sampling
  */
-#define EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW 4
+#define EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW 3
 
 /* Includes ---------------------------------------------------------------- */
 #include <PDM.h>
@@ -97,6 +83,21 @@ void loop()
     }
 
     if (++print_results >= (EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)) {
+      
+        // flash LED if filler is detected
+        float filler_classification = result.classification[0].value;
+        if (filler_classification > 0.6) {
+          Serial.println("FILLER DETECTED");
+          digitalWrite(LEDR, LOW);
+          digitalWrite(LEDG, LOW);
+          digitalWrite(LEDB, LOW);
+        } else {
+          digitalWrite(LEDR, HIGH);
+          digitalWrite(LEDG, HIGH);
+          digitalWrite(LEDB, HIGH);
+        }
+        
+        
         // print the predictions
         ei_printf("Predictions ");
         ei_printf("( DSP: %d ms., Classification: %d ms., Anomaly: %d ms. )",
@@ -106,6 +107,7 @@ void loop()
             ei_printf("    %s: %.5f\n", result.classification[ix].label,
                       result.classification[ix].value);
         }
+        
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
         ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
